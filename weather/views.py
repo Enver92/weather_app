95253c9109116fc81d982b0900be616e4ba236c9
Enver92 <taraqtash@gmail.com>
2018-04-17 00:23:38 +0300

    Added form

--- weather/views.py
+++ weather/views.py
@@ -1,20 +1,27 @@
 from django.shortcuts import render
 
 from .models import City
+from .forms import CityForm
+from .key import key
 import requests
 
 
 def index(request):
-    # key = 'c4e326863862fb255de9d3a95d485d53'
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=c4e326863862fb255de9d3a95d485d53'
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + key
+
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        form.save()
+
+    form = CityForm()
 
     cities = City.objects.all()
-
+    print(cities)
     weather_data = []
 
     for city in cities:
-
-        raw_weather = requests.get(url.format(city)).json()
+        raw_weather = requests.get(url.format(city.name)).json()
+        print(raw_weather)
         city_weather = {
             'city': city.name,
             'temperature': raw_weather['main']['temp'],
@@ -23,5 +30,5 @@ def index(request):
         }
         weather_data.append(city_weather)
 
-    context = {'weather_data': weather_data}
+    context = {'weather_data': weather_data, 'form': form}
     return render(request, 'weather/weather.html', context)
