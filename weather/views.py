ff1bb1502d01ed7f690b4e4bbd8ef9dc1890fd49
Enver92 <taraqtash@gmail.com>
2018-04-16 13:27:50 +0300

    Displays a list of cities

--- weather/views.py
+++ weather/views.py
@@ -1,18 +1,27 @@
 from django.shortcuts import render
 
+from .models import City
 import requests
 
 
 def index(request):
     # key = 'c4e326863862fb255de9d3a95d485d53'
-    url = 'http://api.openweathermap.org/data/2.5/weather?q=Kiev&units=metric&appid=c4e326863862fb255de9d3a95d485d53'
-    city = 'Kiev'
-    raw_weather = requests.get(url.format(city)).json()
-    city_weather = {
-        'city': city,
-        'temperature': raw_weather['main']['temp'],
-        'description': raw_weather['weather'][0]['description'],
-        'icon': raw_weather['weather'][0]['icon'],
-    }
-    context = {'city_weather': city_weather}
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=c4e326863862fb255de9d3a95d485d53'
+
+    cities = City.objects.all()
+
+    weather_data = []
+
+    for city in cities:
+
+        raw_weather = requests.get(url.format(city)).json()
+        city_weather = {
+            'city': city.name,
+            'temperature': raw_weather['main']['temp'],
+            'description': raw_weather['weather'][0]['description'],
+            'icon': raw_weather['weather'][0]['icon'],
+        }
+        weather_data.append(city_weather)
+
+    context = {'weather_data': weather_data}
     return render(request, 'weather/weather.html', context)
